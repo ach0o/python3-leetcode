84f8aa44fc41e13681f27448805b6bd6f8332dc8
achooan <88soldieron@gmail.com>
2019-01-27 23:10:07 +0900

    Add solution to range sum query mutable
    TODO: add solution of segment tree version

---
+++ algorithms/range_sum_query_mutable.py
@@ -0,0 +1,36 @@
+# https://leetcode.com/problems/range-sum-query-mutable/
+
+
+# initial submission
+class NumArray:
+
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.nums = nums
+
+    def update(self, i, val):
+        """
+        :type i: int
+        :type val: int
+        :rtype: void
+        """
+        self.nums[i] = val
+
+    def sumRange(self, i, j):
+        """
+        :type i: int
+        :type j: int
+        :rtype: int
+        """
+        return sum(self.nums[i:j + 1])
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# obj.update(i,val)
+# param_2 = obj.sumRange(i,j)
+
+
+# TODO: Segment tree implementation
