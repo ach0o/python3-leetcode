98bacd0cbc6cff5e182595e38077e87af82af8f4
achooan <88soldieron@gmail.com>
2019-02-03 23:04:27 +0900

    Add solution to length of last word problem

---
+++ algorithms/length_of_last_word.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/length-of-last-word/
+
+
+class Solution:
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if not s.rstrip():
+            return 0
+        return len(s.split()[-1])
