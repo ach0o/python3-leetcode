3ebcdb39f1f1ec5f5ca81dd0989e071af496303c
achooan <88soldieron@gmail.com>
2019-02-09 22:58:47 +0900

    Add solution to single number problem

---
+++ algorithms/single_number.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/single-number/
+
+
+from types import List
+
+
+class Solution:
+    def singleNumber(self, nums: 'List[int]') -> 'int':
+        temp = set()
+        for idx, val in enumerate(nums):
+            if val in temp:
+                temp.remove(val)
+            else:
+                temp.add(val)
+        return temp.pop()
