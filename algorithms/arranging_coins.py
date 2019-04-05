26014884715a68903bd98fb12efc9b663de64948
achooan <88soldieron@gmail.com>
2019-04-05 16:56:18 +0900

    Add solution to arranging coins problem

---
+++ algorithms/arranging_coins.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/arranging-coins/
+
+
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        row = 1
+        while n > 0:
+            row += 1
+            n -= row
+        return row - 1
